2a5588663a966a317326cf74b7454b2b5613ce51
Trevor Pino <trevorpino@yahoo.com>
2020-10-10 14:06:07 -0600

    error fixes

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-// Dependencies
+// Require Dependencies
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
 
-// Initialize app
+// Initialize express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Data parsing
+// Setup data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
-// Routes file
+//Require routes file
 require('./routes/routes')(app);
 
 // Setup listener
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
-}); 
+});  
